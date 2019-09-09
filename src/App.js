715e840be64e33df472f200a1ca1a645728301ae
Daksh <daksh.sagar@yahoo.com>
2019-09-09 14:18:27 +0530

    Add routing support and update menu-item to navigate with withRouter hoc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component'
+import './App.css'
 
 function App() {
   return (
-    <div className='App'>
-      <HomePage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
+    </Router>
   )
 }
 
