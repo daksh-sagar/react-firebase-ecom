e933eb9c4fd9afd18a68ecde85707976ac87f1a3
Daksh <daksh.sagar@yahoo.com>
2019-09-12 17:37:12 +0530

    Create checkout-item component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-import './App.css'
+import CheckoutPage from './pages/checkout/checkout.component'
 import { setCurrentUser } from './redux/user/user.actions'
+import './App.css'
 
 class App extends React.Component {
   unsubscribeFromAuth = null
@@ -41,6 +42,7 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
           <Route exact path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
         </Switch>
       </Router>
     )
