0f0ff48b1d19976f81a1b793f8e39a26af3ab7f9
Daksh <daksh.sagar@yahoo.com>
2019-09-13 23:23:59 +0530

    Add redux-persist and persist cart state to localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore } from 'redux-persist'
 
 import rootReducer from './root-reducer'
 
@@ -6,4 +7,6 @@ const middlewares = []
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
-export default store
+const persistor = persistStore(store)
+
+export { store, persistor }
